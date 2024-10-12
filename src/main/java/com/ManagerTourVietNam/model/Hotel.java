6732b203e260a8f1a672ab589938db0aae4f3d3a
lamtuankiet20122003 <lamtuankiet20122003@gmail.com>
2024-10-12 16:18:47 +0700

    backend vehicles and hotel
    backend vehicles and hotel details

---
+++ src/main/java/com/ManagerTourVietNam/model/Hotel.java
@@ -0,0 +1,72 @@
+package com.ManagerTourVietNam.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    private String id_hotel;
+    private String name_hotel;
+    private String description;
+    private String room;
+    private String type_room;
+    private String image;
+    private Integer status;
+
+    public String getId_hotel() {
+        return id_hotel;
+    }
+
+    public void setId_hotel(String id_hotel) {
+        this.id_hotel = id_hotel;
+    }
+
+    public String getName_hotel() {
+        return name_hotel;
+    }
+
+    public void setName_hotel(String name_hotel) {
+        this.name_hotel = name_hotel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getType_room() {
+        return type_room;
+    }
+
+    public void setType_room(String type_room) {
+        this.type_room = type_room;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/ManagerTourVietNam/service/HotelService.java
@@ -0,0 +1,51 @@
+package com.ManagerTourVietNam.service;
+
+import com.ManagerTourVietNam.model.Hotel;
+import com.ManagerTourVietNam.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class HotelService {
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    //danh sach khach san
+    public List<Hotel> getAllHotel(){
+        return hotelRepository.findAll();
+    }
+
+    //them khach san
+    public Hotel addHotel(Hotel hotel){
+        return hotelRepository.save(hotel);
+    }
+
+    //sua thong tin khach san
+    public Hotel updateHotel(String id,Hotel hotelDetail){
+        Optional<Hotel> optionHotel = hotelRepository.findById(id);
+        if(optionHotel.isPresent()){
+            Hotel hotel=optionHotel.get();
+            hotel.setName_hotel(hotelDetail.getName_hotel());
+            hotel.setDescription(hotelDetail.getDescription());
+            hotel.setRoom(hotelDetail.getRoom());
+            hotel.setType_room(hotelDetail.getType_room());
+            hotel.setImage(hotelDetail.getImage());
+            hotel.setStatus(hotelDetail.getStatus());
+            return hotelRepository.save(hotel);
+        }
+        return null;
+    }
+
+    //Xoa khach san
+    public void deleteHotel(String id){
+        hotelRepository.deleteById(id);
+    }
+
+    //tim kiem khach san
+    public Optional<Hotel> findHotelById(String id){
+        return hotelRepository.findById(id);
+    }
+}
