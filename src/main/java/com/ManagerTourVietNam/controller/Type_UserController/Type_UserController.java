642a5375c963d618f7d377f5da822d0f268dbc44
monaz1234 <Chomon0011@gmail.com>
2024-10-27 13:43:56 +0700

    updatea
    updatea

--- src/main/java/com/ManagerTourVietNam/controller/Type_UserController/Type_UserController.java
+++ src/main/java/com/ManagerTourVietNam/controller/Type_UserController/Type_UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -23,6 +24,24 @@ public class Type_UserController {
     public Type_user addtypeUser(@RequestBody Type_user typeUser ){
         return typeUserService.addType_User(typeUser);
     }
+
+    @PutMapping("api/type_user/{id}")
+    public Type_user edittypeUser(@PathVariable String id, @RequestBody Type_user type_user12){
+        return typeUserService.UpdateType_user(id, type_user12);
+    }
+
+    @DeleteMapping("api/type_user/{id}")
+    public void deletetypeUser(@PathVariable String id){
+        typeUserService.deleteType_User(id);
+    }
+
+
+    @GetMapping("api/type_user/{id}")
+    public Optional<Type_user> findTypeUserById(@PathVariable String id){
+        return typeUserService.findTypeUserById(id);
+    }
+
+
 
     
 }
