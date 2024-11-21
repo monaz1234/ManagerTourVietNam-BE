862e7fdd7991c0fb73ef09caa2eb7197c9ee4810
monaz1234 <Chomon0011@gmail.com>
2024-11-22 01:05:54 +0700

    update
    update

---
+++ src/main/java/com/ManagerTourVietNam/model/BookDetail/BookDetail.java
@@ -0,0 +1,14 @@
+package com.ManagerTourVietNam.model.BookDetail;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "BOOKDETAIL")
+public class BookDetail {
+    @Id
+    @Column
+    
+}
