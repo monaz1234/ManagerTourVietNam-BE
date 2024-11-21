9d781bfbec702b38bd1bd0e6fb89398b81c2d9ae
monaz1234 <Chomon0011@gmail.com>
2024-11-22 01:36:44 +0700

    update
    update

---
+++ src/main/java/com/ManagerTourVietNam/repository/BookDetailReponsitory/BookDetailReponsitory.java
@@ -0,0 +1,7 @@
+package com.ManagerTourVietNam.repository.BookDetailReponsitory;
+
+import com.ManagerTourVietNam.model.BookDetail.BookDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookDetailReponsitory extends JpaRepository<BookDetail, String> {
+}
